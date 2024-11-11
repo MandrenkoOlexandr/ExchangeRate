a4cd3e6c5cf1453724bccd3f42fcdffc6fda1f91
MandrenkoOlexandr <mandrenkos2005@gmail.com>
2024-11-11 17:08:03 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/exchange_rate/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.exchange_rate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.exchange_rate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+    }
+
+
+}
